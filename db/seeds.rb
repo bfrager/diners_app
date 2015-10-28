798ea98d136c61b3f4e16bc7d9a28f10980fae70
bfrager <btfrager@gmail.com>
2015-10-28 10:24:36 -0700

    added 3 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,8 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+  # cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+
+  Restaurant.create({name: 'McDonalds', location: '2nd Street', cuisine: 'fast food', rating: 2})
+  Restaurant.create({name: 'Pie Hole', location: 'Traction Avenue', cuisine: 'dessert', rating: 8})
+  Restaurant.create({name: 'Son of a Gun', location: '3rd Street', cuisine: 'seafood', rating: 9})
