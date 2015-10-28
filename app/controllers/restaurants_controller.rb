ec4b7ee338e30e96d79362aa6fc088cb87a4ba14
bfrager <btfrager@gmail.com>
2015-10-28 10:16:13 -0700

    created controller

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,22 @@
+class RestaurantsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def show
+  end
+end
